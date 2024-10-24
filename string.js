58a52a3f8eebeeea623f038b3ae818bf0240e433
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-10-24 07:55:39 -0700

    Strings in JavaScript
    Strings in JavaScript and also tells the behavior of Strings in Concatenation and also tells the functions of strings in javascript

---
+++ string.js
@@ -0,0 +1,11 @@
+console.log('Now,we are dealing with String Data Type ');
+let fullName = 'Abdul Rehman';
+let fatherName = 'Muhammad Ali';
+//This is String Concatenation :
+console.log(fullName + ' ' + fatherName);
+console.log('My Name is '+fullName+' And My father Name is '+fatherName);
+//functions on Strings 
+console.log(fullName.length);
+console.log(fullName.toLocaleLowerCase());
+console.log(fullName.toLocaleUpperCase());
+console.log(fullName[2]);
