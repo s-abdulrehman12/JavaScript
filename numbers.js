eda1c85b3ca2e832c62e3788a13986ed9f5ff1a2
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-10-25 09:18:00 -0700

    Numbers Concept in Js
    Number type of variable in Java Script

---
+++ numbers.js
@@ -0,0 +1,16 @@
+let numberone = 18;
+let numbertwo = 20; 
+let sum = numberone + numbertwo;
+let subtract = numbertwo - numberone ;
+let mul = numberone * numbertwo;
+let div = numbertwo / numberone;
+console.log("The sum of "+numberone+" And "+numbertwo+" is :"+sum);
+console.log("The Minus of "+numberone+" And "+numbertwo+" is :"+subtract);
+console.log("the product of "+numberone+" and "+numbertwo+" is :"+mul);
+console.log("The divison of "+numberone+" And "+numbertwo+" is :"+div);
+let var1="18";
+let var2="20";
+let final = var1 + var2;
+console.log(final);
+final = var1- var2;
+console.log(final);
