b322a54ad057f3de390266e3e7d1632206d327a2
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-11-01 08:20:58 -0700

    1-Swap Two Numbers in Js
    by using the third variable we swap the two variables

---
+++ Swap2no.js
@@ -0,0 +1,11 @@
+let var1 = 5;
+let var2 = 10;
+console.log('Variables Before swapping ');
+console.log("var1 = " + var1);
+console.log("var2 = " + var2);
+console.log('Variables After the Swapping :');
+let temp = var1;
+var1 = var2;
+var2 = temp;
+console.log("var1 = " + var1);
+console.log("var2 = " + var2);
