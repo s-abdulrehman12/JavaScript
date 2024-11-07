6db34b2c020660ea5c5dd8e2668eb37bc93d954b
Syed Abdul Rehman <151516159+s-abdulrehman12@users.noreply.github.com>
2024-11-07 06:48:15 -0800

    Arithmetic Operators

---
+++ Arithmetic_Operators.js
@@ -0,0 +1,21 @@
+//Arithemetic Operators are the operators which are used to perform operations on the Numbers 
+//Arithemetic Operators are (+,-,*,/,%)
+let a = 10;
+let b = 20;
+
+console.log("a + b :", a+b );
+console.log("a - b :", a-b );
+console.log("a * b :", a*b);
+console.log("a / b :", a/b);
+//Modulus Operator (%) are the operator which are used to gives the Remainder 
+console.log("a % b :",a%b);
+//Increment Opeartor 
+let x = 5; 
+x++;
+// ++ is also equal to x = x+1
+console.log("x++ :",x++);
+//Decrement Operator 
+let y = 6;
+y--;
+// -- is used to decrease the value by 1
+console.log("y-- :",y--);
